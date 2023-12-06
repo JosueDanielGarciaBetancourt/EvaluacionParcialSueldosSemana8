4781573fa1b0706c96aa8b5d121c582a84e954ee
Josué García <77043114@continental.edu.pe>
2023-12-06 12:01:02 -0500

    Avanzando

--- src/logica/ClaseEmpleado.py
+++ src/logica/ClaseEmpleado.py
@@ -1,5 +1,7 @@
 from src.modelo.conexion_db import Conexion
 from tkinter import messagebox
+
+
 class SQLEmpleado:
     def __init__(self, id=None, nombre=None, sueldo=None, cargo=None):
         self.id = id
@@ -15,7 +17,7 @@ class SQLEmpleado:
         try:
             self.cursor.executemany(f"INSERT INTO Empleado VALUES (?, ?, ?, ?)", dato)
             self.conexion.commit()  # Realiza la confirmación para guardar los cambios
-            messagebox.showinfo('Mensaje','Se guardo correctamente')
+            messagebox.showinfo('Mensaje', 'Se guardo correctamente')
         except Exception as e:
             messagebox.showinfo('Mensaje de error', f'Error al guardar: {e}')
         finally:
@@ -34,41 +36,41 @@ class SQLEmpleado:
             self.CerrarConexion()
 
     def EliminarEmpleado(self):
-        dato = (self.id,)
+        dato = (self.id)
         consulta = "DELETE FROM Empleado WHERE id=?"
         try:
             self.cursor.execute(consulta, dato)
             self.conexion.commit()
             print('Empleado eliminado')
         except Exception as e:
-            messagebox.showerror('Error','El empleado tiene boletas asignadas y no se puede eliminar')
+            messagebox.showerror('Error', 'El empleado tiene boletas asignadas y no se puede eliminar')
         finally:
             self.CerrarConexion()
 
     def LeerEmpleado(self):
-            lista = []
-            consulta = ""
-            if self.id:
-                dato = (self.id,)
-                consulta = "SELECT * FROM Empleado WHERE ID = ?"
+        lista = []
+        consulta = ""
+        if self.id:
+            dato = (self.id,)
+            consulta = "SELECT * FROM Empleado WHERE ID = ?"
+        else:
+            dato = (f'%{self.nombre}%',)
+            consulta = "SELECT * FROM Empleado WHERE NombreCompleto LIKE ?"
+
+        try:
+            self.cursor.execute(consulta, dato)
+            resultado = self.cursor.fetchall()
+            if resultado:
+                lista = resultado
+                lista.reverse()
             else:
-                dato = (f'%{self.nombre}%',)
-                consulta = "SELECT * FROM Empleado WHERE NombreCompleto LIKE ?"
+                messagebox.showerror('Mensaje', "Empleado no encontrado.")
+        except Exception as e:
+            print(f'Error al leer empleado: {e}')
+        finally:
+            self.CerrarConexion()
 
-            try:
-                self.cursor.execute(consulta, dato)
-                resultado = self.cursor.fetchall()
-                if resultado:
-                    lista = resultado
-                    lista.reverse()
-                else:
-                    messagebox.showerror('Mensaje', "Empleado no encontrado.")
-            except Exception as e:
-                print(f'Error al leer empleado: {e}')
-            finally:
-                self.CerrarConexion()
-
-            return lista
+        return lista
 
     def DatosCompletos(self):
         self.cursor.execute("Select * from Empleado;")
@@ -77,12 +79,10 @@ class SQLEmpleado:
         lista = Empleado
         lista.reverse()
         return lista
+
     def CerrarConexion(self):
         try:
             self.cursor.close()
             self.conexion.close()
         except Exception as e:
             print(f'Error al cerrar la conexión: {e}')
-
-
-
